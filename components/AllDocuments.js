390dd887a43e2ca121fbfb8f49849d69e111d5c5
shlomomdahan <64103471+shlomomdahan@users.noreply.github.com>
2023-06-22 10:05:34 -0400

    Add list/grid button functionality

--- components/AllDocuments.js
+++ components/AllDocuments.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {signOut} from "next-auth/react";
 
-const AllCollections = (props) => {
+const AllDocuments = (props) => {
 
   console.log(props.user);
 
@@ -26,6 +26,10 @@ const AllCollections = (props) => {
     },
   ]
 
+  // Set state variable for showing grid view
+  const [viewMode, setViewMode] = useState('list');
+
+
   return (
     <div className="allcollections p-12">
       {/*<button*/}
@@ -39,11 +43,11 @@ const AllCollections = (props) => {
           Sign out
         </button>
         <div className="bg-gray-200 text-sm text-gray-500 leading-none border-2 border-gray-200 rounded-full inline-flex">
-          <button className="inline-flex items-center transition-colors duration-300 ease-in focus:outline-none hover:text-blue-400 focus:text-blue-400 rounded-l-full px-4 py-2 active" id="grid">
+          <button className={`inline-flex items-center transition-colors duration-300 ease-in focus:outline-none hover:text-blue-400 focus:text-blue-400 rounded-l-full px-4 py-2 ${viewMode === 'grid' ? 'active' : ''}`} onClick={() => setViewMode('grid')} id="grid">
            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="fill-current w-4 h-4 mr-2"><rect x="3" y="3" width="7" height="7"></rect><rect x="14" y="3" width="7" height="7"></rect><rect x="14" y="14" width="7" height="7"></rect><rect x="3" y="14" width="7" height="7"></rect></svg>
            <span>Grid</span>
           </button>
-          <button className="inline-flex items-center transition-colors duration-300 ease-in focus:outline-none hover:text-blue-400 focus:text-blue-400 rounded-r-full px-4 py-2" id="list">
+          <button className={`inline-flex items-center transition-colors duration-300 ease-in focus:outline-none hover:text-blue-400 focus:text-blue-400 rounded-r-full px-4 py-2 ${viewMode === 'list' ? 'active' : ''}`} onClick={() => setViewMode('list')} id="list">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="fill-current w-4 h-4 mr-2"><line x1="8" y1="6" x2="21" y2="6"></line><line x1="8" y1="12" x2="21" y2="12"></line><line x1="8" y1="18" x2="21" y2="18"></line><line x1="3" y1="6" x2="3.01" y2="6"></line><line x1="3" y1="12" x2="3.01" y2="12"></line><line x1="3" y1="18" x2="3.01" y2="18"></line></svg>
             <span>List</span>
           </button>
@@ -52,19 +56,43 @@ const AllCollections = (props) => {
 
       {/*<h4>User session:</h4>*/}
       {/*<pre>{JSON.stringify(props.user, null, 2)}</pre>*/}
-      <ul className="divide-y divide-gray-200">
+      {viewMode === "list" && <ul className="divide-y divide-gray-200">
         {collection.map((document) => (
-          <li key={document.name} className="py-4 flex">
-            <img className="h-20 w-20" src={document.image} alt="" />
-            <div className="ml-3">
+            <>
+              <li data-tooltip-target="tooltip-default" key={document.name} className="py-4 flex hover:bg-gray-100">
+                <img className="h-20 w-20" src={document.image} alt="" />
+                <div className="ml-3">
+                  <p className="text-sm font-medium text-gray-900">{document.name}</p>
+                  <p className="text-sm text-gray-500">{document.format}</p>
+                </div>
+              </li>
+              {/*<div id="tooltip-default" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">*/}
+              {/*  {document.name}*/}
+              {/*<div class="tooltip-arrow" data-popper-arrow={true}></div>*/}
+              {/*</div>*/}
+            </>
+        ))}
+      </ul>}
+      {viewMode === "grid" && <ul className="grid grid-cols-4 gap-4">
+        {collection.map((document) => (
+          <li key={document.name} className="py-4 flex flex-col items-center hover:bg-gray-100">
+            <img className="h-40 w-40" src={document.image} alt="" />
+            <div className="ml-2">
               <p className="text-sm font-medium text-gray-900">{document.name}</p>
               <p className="text-sm text-gray-500">{document.format}</p>
             </div>
           </li>
         ))}
-      </ul>
+      </ul>}
     </div>
   );
 }
 
-export default AllCollections;
+// <div class="grid grid-cols-4 gap-4">
+//   <div>01</div>
+//   <!-- ... -->
+//   <div>09</div>
+// </div>
+
+
+export default AllDocuments;
