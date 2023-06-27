d09ddb62e7e3c4b10cc5ca99af3201da77a47869
shlomomdahan <64103471+shlomomdahan@users.noreply.github.com>
2023-06-27 10:06:34 -0400

    fixed class - className in TableGrid

--- components/AllDocuments/TableGrid.js
+++ components/AllDocuments/TableGrid.js
@@ -3,7 +3,7 @@ import {useState} from "react";
 const TableGrid = (props) => {
     return (
         <>
-            <div class="flex items-center justify-center py-4 md:py-8 flex-wrap">
+            <div className="flex items-center justify-center py-4 md:py-8 flex-wrap">
                 <button type="button"
                         className="text-blue-700 hover:text-white border border-blue-600 bg-white hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-full text-base font-medium px-5 py-2.5 text-center mr-3 mb-3 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:bg-gray-900 dark:focus:ring-blue-800">
                     All categories
@@ -37,7 +37,7 @@ const TableGrid = (props) => {
                     Credentials
                 </button>
             </div>
-            <div class="grid grid-cols-2 md:grid-cols-3 gap-4">
+            <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
                 <div>
                     <img className="h-auto max-w-full rounded-lg"
                          src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg" alt="" />
