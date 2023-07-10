e39124f117f9b6b4d1e4a963a70ad0605cd71b73
shlomomdahan <64103471+shlomomdahan@users.noreply.github.com>
2023-07-10 10:04:11 -0600

    recaptcha not yet working / added about page

--- pages/api/contactUs.js
+++ pages/api/contactUs.js
@@ -1,80 +1,132 @@
+// import axios from "axios";
+// import qs from 'qs';
+//
+// const verifyRecaptcha = async (token) => {
+//
+//   const secretKey = process.env.NEXT_PUBLIC_RECAPTCHA_SECRET_KEY;
+//
+//   const verificationUrl =
+//     // `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${token}`;
+//   `https://recaptchaenterprise.googleapis.com/v1/projects/${process.env.NEXT_PUBLIC_GOOGLE_PROJECT_ID}/assessments?key=${process.env.NEXT_PUBLIC_GOOGLE_RECAPTCHA_API_KEY}`
+//
+//   // const encodedData = new URLSearchParams();
+//   // encodedData.append('secret', secretKey);
+//   // encodedData.append('response', token);
+//
+//   return axios.post(
+//     verificationUrl, {
+//       "event": {
+//       "token": token,
+//           "siteKey": secretKey,
+//           "expectedAction": "enquiryFormSubmit"
+//       }
+//     },{
+//     headers: {
+//       'Content-Type': "application/json"
+//     }
+//   });
+// };
+//
+// export default async function handler(req, res) {
+//   try {
+//     const gRecaptchaToken = req.body.gRecaptchaToken;
+//     const email = req.body.email;
+//     const subject = req.body.subject;
+//     const message = req.body.message;
+//
+//     // Recaptcha response
+//     const response = await verifyRecaptcha(gRecaptchaToken);
+//
+//     // Checking if the response sent by reCaptcha success or not and if the score is above 0.5
+//     // In ReCaptcha v3, a score sent which tells if the data sent from front end is from Human or from Bots. If score above 0.5 then it is human otherwise it is bot
+//     // For more info check, https://developers.google.com/recaptcha/docs/v3
+//     // ReCaptcha v3 response, {
+//     //     "success": true|false,      // whether this request was a valid reCAPTCHA token for your site
+//     //     "score": number             // the score for this request (0.0 - 1.0)
+//     //     "action": string            // the action name for this request (important to verify)
+//     //     "challenge_ts": timestamp,  // timestamp of the challenge load (ISO format yyyy-MM-dd'T'HH:mm:ssZZ)
+//     //     "hostname": string,         // the hostname of the site where the reCAPTCHA was solved
+//     //     "error-codes": [...]        // optional
+//     //   }
+//
+//     if (response.data.success) {
+//       axios
+//         .post("https://getform.io/f/c9a0111b-5cf1-4a2a-a27d-fb7beb55420f", {
+//             data: {email: email, subject: subject, message: message},
+//           },
+//           {
+//             // headers: {
+//             //   Accept: "application/json",
+//             //   "Content-Type": "application/json",
+//             //   "Access-Control-Allow-Origin": process.env.NEXTAUTH_URL,
+//             // }
+//           })
+//         .then(res => console.log(res))
+//         .catch(error => console.log(error));
+//
+//     // if (req) {
+//     //   return res.json({
+//     //     req: response.data,
+//     //     email: req.body.email
+//     //   });
+//     } else {
+//       return res.json({
+//         status: "error",
+//         message: "ReCaptcha went wrong, please try again",
+//         response: response.data,
+//         // email: email
+//       });
+//     }
+//   } catch (error) {
+//     res.json({
+//       status: "error",
+//       message: "Something went wrong, please try again"
+//     });
+//   }
+// };
+
 import axios from "axios";
-import qs from 'qs';
 
 const verifyRecaptcha = async (token) => {
+    const verificationUrl = `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.NEXT_PUBLIC_RECAPTCHA_SECRET_KEY}&response=${token}`
 
-  const secretKey = process.env.NEXT_PUBLIC_RECAPTCHA_SECRET_KEY;
-
-  const verificationUrl =
-    `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${token}`;
-
-  // const encodedData = new URLSearchParams();
-  // encodedData.append('secret', secretKey);
-  // encodedData.append('response', token);
-
-  return axios.post(
-    verificationUrl, {
-    },{
-    headers: {
-      'Content-Type': "application/x-www-form-urlencoded"
-    }
-  });
+    return axios.post(verificationUrl);
 };
 
 export default async function handler(req, res) {
-  try {
-    const gRecaptchaToken = req.body.gRecaptchaToken;
-    const email = req.body.email;
-    const subject = req.body.subject;
-    const message = req.body.message;
+    try {
+        const gRecaptchaToken = req.body.gRecaptchaToken;
+        const email = req.body.email;
+        const subject = req.body.subject;
+        const message = req.body.message;
 
-    // Recaptcha response
-    const response = await verifyRecaptcha(gRecaptchaToken);
+        const response = await verifyRecaptcha(gRecaptchaToken);
 
-    // Checking if the response sent by reCaptcha success or not and if the score is above 0.5
-    // In ReCaptcha v3, a score sent which tells if the data sent from front end is from Human or from Bots. If score above 0.5 then it is human otherwise it is bot
-    // For more info check, https://developers.google.com/recaptcha/docs/v3
-    // ReCaptcha v3 response, {
-    //     "success": true|false,      // whether this request was a valid reCAPTCHA token for your site
-    //     "score": number             // the score for this request (0.0 - 1.0)
-    //     "action": string            // the action name for this request (important to verify)
-    //     "challenge_ts": timestamp,  // timestamp of the challenge load (ISO format yyyy-MM-dd'T'HH:mm:ssZZ)
-    //     "hostname": string,         // the hostname of the site where the reCAPTCHA was solved
-    //     "error-codes": [...]        // optional
-    //   }
+        if (
+            response.data.success &&
+            response.data.score > 0.5
+        ) {
 
-    if (!response.data.success) {
-      // axios
-      //   .post("https://getform.io/f/c9a0111b-5cf1-4a2a-a27d-fb7beb55420f", {
-      //       data: {email: email, subject: subject, message: message},
-      //     },
-      //     {
-      //       // headers: {
-      //       //   Accept: "application/json",
-      //       //   "Content-Type": "application/json",
-      //       //   "Access-Control-Allow-Origin": process.env.NEXTAUTH_URL,
-      //       // }
-      //     })
-      //   .then(res => console.log(res))
-      //   .catch(error => console.log(error));
+            axios.post("https://getform.io/f/c9a0111b-5cf1-4a2a-a27d-fb7beb55420f",
+                {
+                    data: {email: email, subject: subject, message: message},
+                })
+                .then(res => console.log(res))
+                .catch(error => console.log(error));
 
-    // if (req) {
-      return res.json({
-        req: response.data,
-        email: req.body.email
-      });
-    } else {
-      return res.json({
-        status: "error",
-        message: "ReCaptcha went wrong, please try again",
-        // response: response,
-        // email: email
-      });
+        } else {
+            return res.json({
+                status: "error",
+                message: "ReCaptcha went wrong, please try again",
+                response: response.data,
+            });
+        }
+    } catch (error) {
+        console.log(error.response.data);
+        res.json({
+            status: "error",
+            message: "Something went wrong, please try again",
+            error: error.message,
+        });
     }
-  } catch (error) {
-    res.json({
-      status: "error",
-      message: "Something went wrong, please try again"
-    });
-  }
 };
--- pages/contact.js
+++ pages/contact.js
@@ -18,34 +18,34 @@ const Contact = (props) => {
     const [token, setToken] = useState('');
 
     // const [noOfVerifications, setNoOfVerifications] = useState(0);
-    const [dynamicAction, setDynamicAction] = useState('homepage');
+    // const [dynamicAction, setDynamicAction] = useState('homepage');
     // const [actionToChange, setActionToChange] = useState('');
 
-    const clickHandler = useCallback(async () => {
-        if (!executeRecaptcha) {
-            return;
-        }
+    // const clickHandler = useCallback(async () => {
+    //     if (!executeRecaptcha) {
+    //         return;
+    //     }
+    //
+    //     const result = await executeRecaptcha('dynamicAction');
+    //
+    //     setToken(result);
+    //     // setNoOfVerifications(noOfVerifications => noOfVerifications + 1);
+    // }, [dynamicAction, executeRecaptcha]);
 
-        const result = await executeRecaptcha('dynamicAction');
-
-        setToken(result);
-        // setNoOfVerifications(noOfVerifications => noOfVerifications + 1);
-    }, [dynamicAction, executeRecaptcha]);
-
-    useEffect(() => {
-        if (!executeRecaptcha || !dynamicAction) {
-            return;
-        }
-
-        let handleReCaptchaVerify;
-        handleReCaptchaVerify = async () => {
-            const token = await executeRecaptcha(dynamicAction);
-            setToken(token);
-            // setNoOfVerifications(noOfVerifications => noOfVerifications + 1);
-        };
-
-        handleReCaptchaVerify();
-    }, [executeRecaptcha, dynamicAction]);
+    // useEffect(() => {
+    //     if (!executeRecaptcha || !dynamicAction) {
+    //         return;
+    //     }
+    //
+    //     let handleReCaptchaVerify;
+    //     handleReCaptchaVerify = async () => {
+    //         const token = await executeRecaptcha(dynamicAction);
+    //         setToken(token);
+    //         // setNoOfVerifications(noOfVerifications => noOfVerifications + 1);
+    //     };
+    //
+    //     handleReCaptchaVerify();
+    // }, [executeRecaptcha, dynamicAction]);
 
     const handleSubmitForm = useCallback((e) => {
         e.preventDefault();
@@ -55,7 +55,7 @@ const Contact = (props) => {
           return;
         }
 
-        executeRecaptcha("enquiryFormSubmit").then((gReCaptchaToken) => {
+        executeRecaptcha("submit").then((gReCaptchaToken) => {
             console.log(gReCaptchaToken, "response Google reCaptcha server");
             setToken(gReCaptchaToken);
             submitEnquiryForm(gReCaptchaToken);
@@ -73,11 +73,6 @@ const Contact = (props) => {
             subject: subject,
             message: message,
             gReCaptchaToken: gReCaptchaToken,
-        }, {
-            // headers: {
-            //     Accept: "application/x-www-form-urlencoded",
-            //     "Content-Type": "application/x-www-form-urlencoded",
-            // }
         }).then(async (res) => {
             console.log(res);
             console.log(res.data);
@@ -107,56 +102,75 @@ const Contact = (props) => {
                     <div className="sidebar w-64 bg-gray-100 flex-shrink-0">
                         <Sidebar />
                     </div>
-                    <div className="content flex-grow bg-white p-6">
-                        <h2 className="text-xl font-bold mb-2">Contact Us</h2>
-                        <form
-                          onSubmit={handleSubmitForm}
-                          className="space-y-4"
-                        >
-                            <input
-                                onChange={(e) => {
-                                    setEmail(e?.target?.value)
-                                }}
-                                value={email}
-                                id="email"
-                                type="email"
-                                name="email"
-                                placeholder="Email"
-                                className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
-                                required
-                            />
-                            <input
-                                onChange={(e) => {
-                                    setSubject(e?.target?.value)
-                                }}
-                                value={subject}
-                                id="subject"
-                                type="text"
-                                name="subject"
-                                placeholder="Subject"
-                                className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
-                                required
-                            />
-                            <textarea
-                                onChange={(e) => {
-                                    setMessage(e?.target?.value);
-                                }}
-                                value={message}
-                                id="message"
-                                name="message"
-                                placeholder="Enter your message"
-                                className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
-                                required
-                            />
-                            <button
-                              type="submit"
-                              disabled={loading}
-                              className={`py-2 px-4 bg-indigo-500 text-white font-semibold rounded-lg shadow-md hover:bg-indigo-700 focus:outline-none ${loading ? 'opacity-50 cursor-not-allowed' : ''}`}>
-                                Send Message
-                            </button>
-                            {token && <p>Token: {token}</p>}
-                        </form>
-                    </div>
+                    <section class="bg-white dark:bg-gray-900 w-full">
+                        <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+                            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Us</h2>
+                            <form
+                                onSubmit={handleSubmitForm}
+                                className="space-y-8"
+                            >
+                                <div className={"py-1"}>
+                                    <label htmlFor="email"
+                                           className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your
+                                        email</label>
+                                    <input
+                                        onChange={(e) => {
+                                            setEmail(e?.target?.value)
+                                        }}
+                                        value={email}
+                                        id="email"
+                                        type="email"
+                                        name="email"
+                                        placeholder="name@email.com"
+                                        className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                                        required
+                                    />
+                                </div>
+                                
+                                <div className={"py-1"}>
+                                    <label htmlFor="subject"
+                                           className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Subject</label>
+                                    <input
+                                        onChange={(e) => {
+                                            setSubject(e?.target?.value)
+                                        }}
+                                        value={subject}
+                                        id="subject"
+                                        type="text"
+                                        name="subject"
+                                        placeholder="Let us know how we can help you"
+                                        className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                                        required
+                                    />
+                                </div>
+
+                                <div className={"py-1"}>
+                                    <label htmlFor="message"
+                                           className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your
+                                        message</label>
+                                    <textarea
+                                        onChange={(e) => {
+                                            setMessage(e?.target?.value);
+                                        }}
+                                        value={message}
+                                        id="message"
+                                        name="message"
+                                        placeholder="Leave a comment"
+                                        className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                        required
+                                    />
+                                </div>
+
+                                <button
+                                    type="submit"
+                                    disabled={loading}
+                                    className={`py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 ${loading ? 'opacity-50 cursor-not-allowed' : ''}`}>
+                                    Send Message
+                                </button>
+                                {token && <p>Token: {token}</p>}
+                            </form>
+                        </div>
+                    </section>
                 </div>
             </div>
             <ToastContainer position="bottom-right" autoClose={5000} />
