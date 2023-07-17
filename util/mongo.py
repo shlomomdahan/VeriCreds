cb2ad2ad9439a8c937b917feb131d2ed0bfd1b01
shlomomdahan <64103471+shlomomdahan@users.noreply.github.com>
2023-07-17 10:14:45 -0400

    get-database function

--- util/mongo.py
+++ util/mongo.py
@@ -4,10 +4,22 @@ import urllib.parse
 
 config = dotenv_values("../.env")
 
-username = urllib.parse.quote_plus(config['MONGO_DB_USERNAME'])
-password = urllib.parse.quote_plus(config['MONGO_DB_PASSWORD'])
 
-client = MongoClient(f"mongodb+srv://{username}:{password}@vericreds.gjtihuc.mongodb.net/vericreds-db")
+def get_database():
+    # Provide the mongodb atlas url to connect python to mongodb using pymongo
+    username = urllib.parse.quote_plus(config['MONGO_DB_USERNAME'])
+    password = urllib.parse.quote_plus(config['MONGO_DB_PASSWORD'])
 
-db = client["vericreds-db"]
-users = db["users"]
+    connection_string = f"mongodb+srv://{username}:{password}@vericreds.gjtihuc.mongodb.net/vericreds-db"
+
+    # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
+    client = MongoClient(connection_string)
+
+    # Create the database for our example (we will use the same database throughout the tutorial
+    return client["vericreds-db"]
+
+
+# This is added so that many files can reuse the function get_database()
+if __name__ == "__main__":
+    # Get the database
+    dbname = get_database()
